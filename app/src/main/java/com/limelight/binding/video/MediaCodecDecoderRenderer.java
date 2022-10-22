b0bdf59481541f03a14e7a6cc055c49cf62aa259
skbeh <60107333+skbeh@users.noreply.github.com>
2022-10-22 23:24:29 +0800

    Use BT.709 and full color range as default
    The change can improve color quality and avoid unnecessary conversion.

--- app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
+++ app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
@@ -344,7 +344,7 @@ public class MediaCodecDecoderRenderer extends VideoDecoderRenderer implements C
     }
 
     public int getPreferredColorRange() {
-        return MoonBridge.COLOR_RANGE_LIMITED;
+        return MoonBridge.COLOR_RANGE_FULL;
     }
 
     public void notifyVideoForeground() {
